465610c9d786a46183d5e1e32df060feaffbb798
burgues0 <victordpp@outlook.com>
2025-06-24 01:50:55 -0300

    add: greetings, debian and config checks

--- install.sh
+++ install.sh
@@ -1,12 +1,76 @@
 #!/bin/bash
 
-#initial logic                                          FIRST
-#> check if current distro is debian-based
-#>> if it is not, cancel the script alltogether
+ROOT_DIR='.'
+CONFIG_FILE='config.yaml'
+CONFIG_PATH="$ROOT_DIR/$CONFIG_FILE"
 
-#> check if debian-apps.yaml config file exists (function)      SECOND
-#>> if exists, check structure to see if it is ok or not
-#>> if not, stop the script "missing config.yaml file"
+greetings(){
+    echo "=== Debian Dotfiles ===."
+    echo "> Created by burgues0."
+    echo "> Please clone the repository and alter the script the way it suits you best."
+    echo "> Check the README.md for references on how to tinker with the script."
+    echo ""
+    #sleep 3
+    #clear
+}
+
+print_success(){
+    echo -e "> \033[1;32m$1\033[0m"
+}
+
+print_error(){
+    echo -e "> \033[1;31m$1\033[0m"
+}
+
+check_debian(){
+    grep -iE 'debian|buntu|mint' /etc/*release > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        print_error "Error: current distro is not Debian based."
+        exit 1
+    fi
+    print_success "Success: current distro is Debian based."
+}
+
+check_config_file(){
+    ext=${CONFIG_FILE##*.}
+    if [ ! -f "$CONFIG_PATH" ]; then
+        print_error "Error: config.yaml file does not exist in the project's root folder."
+        exit 1
+    fi
+    if [ "$ext" != "yaml" ]; then
+        print_error "Error: config.yaml file is not of type 'yaml'."
+        exit 1
+    fi
+    print_success "Success: file $CONFIG_FILE exists in the project's root folder."
+}
+
+validate_yaml(){
+    dpkg -s yamllint > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        print_error "Error: yamllint is not installed."
+        print_error "Please install it with 'sudo apt install yamllint'."
+        exit 1
+    fi
+    print_success "Success: package yamllint is installed."
+
+    yamllint $CONFIG_PATH > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        print_error "Error: invalid .yaml formatting."
+        print_error "Please run 'yamllint $CONFIG_PATH' to check for errors."
+        exit 1
+    fi
+    print_success "Success: $CONFIG_FILE is formatted correctly."
+}
+
+# FIRST
+greetings
+echo "+ Initializing Dotfiles +"
+check_debian
+
+# SECOND
+echo "+ Validating Configuration Files +"
+check_config_file
+validate_yaml
 
 #> check internet connection because of updates + installations (function)      THIRD (define functions to be used in main logic)
 #> create file structure (function)
